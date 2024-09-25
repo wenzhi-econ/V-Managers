14e3945001aa57470fa786cb5c7af312d6225471
Wenzhi Wang <wangwz.econ@gmail.com>
2024-09-25 10:00:40 -0500

    Modify word cloud codes for LDA

--- 050401SkillsDescriptionLDA.py
+++ 050401SkillsDescriptionLDA.py
@@ -1,3 +1,9 @@
+#! python3
+
+# ??#??#??#??#??#??#??#??#??#??#??#??#??#??#??#??#??#??#??#??#??#??
+# ?? step 0. configuration
+# ??#??#??#??#??#??#??#??#??#??#??#??#??#??#??#??#??#??#??#??#??#??
+
 import os
 import pandas as pd
 import numpy as np
@@ -6,19 +12,17 @@ from sklearn.decomposition import LatentDirichletAllocation
 from wordcloud import WordCloud
 import matplotlib.pyplot as plt
 
-# ??#??#??#??#??#??#??#??#??#??#??#??#??#??#??#??#??#??#??#??#??#??
-# ?? step 1. transformation of the dataset (turn ind-skill level to ind level)
-# ??#??#??#??#??#??#??#??#??#??#??#??#??#??#??#??#??#??#??#??#??#??
-
 path_skills = (
     "E:/__RA/02MANAGERS/Paper Managers/Data/01RawData/01MNEData/SkillsInput.dta"
 )
 path_output_data = (
     "E:/__RA/02MANAGERS/Paper Managers/Data/02TempData/temp_SkillsAfterLDA.dta"
 )
-path_output_fig = (
-    "E:/__RA/02MANAGERS/Paper Managers/Results"
-)
+path_output_fig = "E:/__RA/02MANAGERS/Paper Managers/Results"
+
+# ??#??#??#??#??#??#??#??#??#??#??#??#??#??#??#??#??#??#??#??#??#??
+# ?? step 1. transformation of the dataset (turn ind-skill level to ind level)
+# ??#??#??#??#??#??#??#??#??#??#??#??#??#??#??#??#??#??#??#??#??#??
 
 data = pd.read_stata(path_skills)
 
@@ -44,10 +48,12 @@ vectorizer = CountVectorizer()
 skills_matrix = vectorizer.fit_transform(skills_combined["Skills"])
 
 # -?#-?#-?#-?#-?#-?#-?#-?#-?#-?#-?#-?#-?#-?#-?#-?#-?#-?#-?#-?#-?#-?
-# -? s-2-1. Fit LDA model and get topic distributions for each individual
+# -? s-2-1. Fit 3-topic LDA model
+# -?        and get topic distributions for each individual
 # -?#-?#-?#-?#-?#-?#-?#-?#-?#-?#-?#-?#-?#-?#-?#-?#-?#-?#-?#-?#-?#-?
 
-#!! Function to fit LDA model and get topic distributions
+
+#!! A function to fit LDA model and get topic distributions
 def fit_lda_and_get_topics(n_topics, skills_matrix):
     lda_model = LatentDirichletAllocation(
         n_components=n_topics, random_state=42
@@ -60,63 +66,44 @@ def fit_lda_and_get_topics(n_topics, skills_matrix):
 
     return lda_model
 
-#!! Fit the LDA model with 2 topics
-lda_2_topics = fit_lda_and_get_topics(2, skills_matrix)
 
 #!! Fit the LDA model with 3 topics
 lda_3_topics = fit_lda_and_get_topics(3, skills_matrix)
-
-#!! Fit the LDA model with 5 topics
-lda_5_topics = fit_lda_and_get_topics(5, skills_matrix)
 
 #!! Fit the LDA model with 3 topics
 skills_combined[
     [
         "IDlse",
-        "Topic1_2",
-        "Topic2_2",
         "Topic1_3",
         "Topic2_3",
         "Topic3_3",
-        "Topic1_5",
-        "Topic2_5",
-        "Topic3_5",
-        "Topic4_5",
-        "Topic5_5",
     ]
 ].to_stata(path_output_data)
 
-# ??#??#??#??#??#??#??#??#??#??#??#??#??#??#??#??#??#??#??#??#??#??
-# ?? step 3. Describe the resulting topics
-# ??#??#??#??#??#??#??#??#??#??#??#??#??#??#??#??#??#??#??#??#??#??
+# -?#-?#-?#-?#-?#-?#-?#-?#-?#-?#-?#-?#-?#-?#-?#-?#-?#-?#-?#-?#-?#-?
+# -? s-2-2. Describe the resulting topic 
+# -?        (word distribution) using word cloud
+# -?#-?#-?#-?#-?#-?#-?#-?#-?#-?#-?#-?#-?#-?#-?#-?#-?#-?#-?#-?#-?#-?
 
-# Extract the top words for each topic
-def get_top_words(lda_model, feature_names, n_top_words):
+
+# !! A function to extract the top words for each topic
+def get_top_words(model, feature_names, n_top_words):
     top_words = {}
-    for topic_idx, topic in enumerate(lda_model.components_):
-        top_words[f"Topic {topic_idx + 1}"] = [
-            feature_names[i] for i in topic.argsort()[: -n_top_words - 1 : -1]
-        ]
+    for topic_idx, topic in enumerate(model.components_):
+        top_words_for_topic = {
+            feature_names[i]: topic[i]
+            for i in topic.argsort()[: -n_top_words - 1 : -1]
+        }
+        top_words[f"Topic {topic_idx + 1}"] = top_words_for_topic
     return top_words
 
-n_top_words = 20
-feature_names = vectorizer.get_feature_names_out()
 
-# Get top words for 2-topic model
-top_words_2_topics = get_top_words(lda_2_topics, feature_names, n_top_words)
-
-# Get top words for 3-topic model
-top_words_3_topics = get_top_words(lda_3_topics, feature_names, n_top_words)
-
-# Get top words for 5-topic model
-top_words_5_topics = get_top_words(lda_5_topics, feature_names, n_top_words)
-
-# Function to generate word clouds for topics
+# !! A function to generate word clouds for topics
 def generate_word_clouds(top_words, filepath, model_type):
-    for topic, words in top_words.items():
+    for topic, words_probs in top_words.items():
         wordcloud = WordCloud(
             width=800, height=400, background_color="white"
-        ).generate(" ".join(words))
+        ).generate_from_frequencies(words_probs)
         plt.figure()
         plt.imshow(wordcloud, interpolation="bilinear")
         plt.axis("off")
@@ -127,11 +114,28 @@ def generate_word_clouds(top_words, filepath, model_type):
         plt.close()
 
 
-# Generate word clouds for 2-topic model
-generate_word_clouds(top_words_2_topics, path_output_fig, "LDA2")
+# !! Get top words for 3-topic model
+feature_names = vectorizer.get_feature_names_out()
+top_words_3_topics_50 = get_top_words(lda_3_topics, feature_names, 50)
+top_words_3_topics_1000 = get_top_words(lda_3_topics, feature_names, 1000)
 
-# Generate word clouds for 3-topic model
-generate_word_clouds(top_words_3_topics, path_output_fig, "LDA3")
+# !! Generate word clouds for 3-topic model
+generate_word_clouds(top_words_3_topics_50, path_output_fig, "LDA3_50Words")
+generate_word_clouds(top_words_3_topics_1000, path_output_fig, "LDA3_1000Words")
 
-# Generate word clouds for 5-topic model
-generate_word_clouds(top_words_5_topics, path_output_fig, "LDA5")
+# ??#??#??#??#??#??#??#??#??#??#??#??#??#??#??#??#??#??#??#??#??#??
+# ?? step 3. Test 2- and 5-Topic LDA analysis
+# ??#??#??#??#??#??#??#??#??#??#??#??#??#??#??#??#??#??#??#??#??#??
+
+
+#!! Fit the LDA model with 2 and 5 topics
+lda_2_topics = fit_lda_and_get_topics(2, skills_matrix)
+lda_5_topics = fit_lda_and_get_topics(5, skills_matrix)
+
+# !! Get top words for 2- and 5-topic model
+top_words_2_topics_1000 = get_top_words(lda_2_topics, feature_names, 1000)
+top_words_5_topics_1000 = get_top_words(lda_5_topics, feature_names, 1000)
+
+# !! Generate word clouds for 2- and 5-topic model
+generate_word_clouds(top_words_2_topics_1000, path_output_fig, "LDA2_1000Words")
+generate_word_clouds(top_words_5_topics_1000, path_output_fig, "LDA5_1000Words")
